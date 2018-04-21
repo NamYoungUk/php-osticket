7d6380e8975971a06adbc7884e9f69e2e6e1b87e
Nam Young Uk <namyounguk@gmail.com>
2018-04-21 12:49:30 +0900

    Update ost-config.php

--- include/ost-config.php
+++ include/ost-config.php
@@ -39,9 +39,9 @@ define('ADMIN_EMAIL','demouser@opsgility.com');
 # ---------------------------------------------------
 # Mysql Login info
 define('DBTYPE','mysql');
-define('DBHOST','yourservername.mysql.database.azure.com');
+define('DBHOST','bootcampmysql-namyu.mysql.database.azure.com');
 define('DBNAME','osticket');
-define('DBUSER','demouser@yourservername');
+define('DBUSER','demouser@bootcampmysql-namyu');
 define('DBPASS','demo@pass123');
 
 # Table prefix
